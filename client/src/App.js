5684dc39a0ef7fad76d4931d8697fe96a8fe651a
AyaBenkabbour <ayabenkabbour0@gmail.com>
2023-12-11 07:44:53 +0100

    established connexion w/ be

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
-import Landing from './Landing';
+import "./App.css";
+import Landing from "./Landing";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/members")
+      .then((response) => setMembers(response.data.members))
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, []);
+
   return (
-    <div className="App">
-      <Landing/>
+    <div>
+      <h1>Members</h1>
+      <ul>
+        {members.map((member, index) => (
+          <li key={index}>{member}</li>
+        ))}
+      </ul>
     </div>
   );
 }
